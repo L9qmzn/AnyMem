69d3ba3a67179d8ce8ad8a68b577d608687d501d
Admin <55500771+L9qmzn@users.noreply.github.com>
2025-11-18 15:51:16 +0800

    chore: 语言切换的导致的颤抖问题

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,11 +3,12 @@ import { observer } from "mobx-react-lite";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { RouterProvider } from "react-router-dom";
-import "./i18n";
+import i18n from "./i18n";
 import "./index.css";
 import router from "./router";
 // Configure MobX before importing any stores
 import "./store/config";
+import { instanceStore } from "./store";
 import { initialInstanceStore } from "./store/instance";
 import { initialUserStore } from "./store/user";
 import { applyThemeEarly } from "./utils/theme";
@@ -26,6 +27,11 @@ const Main = observer(() => (
 (async () => {
   await initialInstanceStore();
   await initialUserStore();
+
+  // Set i18n language before rendering to prevent flash of wrong language
+  const initialLocale = instanceStore.state.locale;
+  await i18n.changeLanguage(initialLocale);
+  document.documentElement.setAttribute("lang", initialLocale);
 
   const container = document.getElementById("root");
   const root = createRoot(container as HTMLElement);
